1ecc976b81fcd1378553686ca58946534230e1fd
Getonto3 <getontomoses18@gmail.com>
2024-02-06 19:10:57 +0300

    feat: project set up

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    home: Ninja(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Ninja extends StatelessWidget {
+  const Ninja({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+          "Ninja ID Card",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/Minions.jpg"),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                  color: Colors.grey, fontSize: 20.0, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Getonto",
+              style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                  color: Colors.grey, fontSize: 20.0, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "8",
+              style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Row(children: [
+              Icon(Icons.mail, color: Colors.grey),
+              SizedBox(width: 10.0),
+              Text("example@gmail.com",
+                  style: TextStyle(color: Colors.grey[400], fontSize: 20.0))
+            ]),
+          ],
         ),
       ),
     );
